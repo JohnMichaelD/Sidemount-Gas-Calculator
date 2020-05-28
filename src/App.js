b386e68c7abe7cc397f125b8bdc9cefd5e602574
John Darrin <john.m.darrin@gmail.com>
2020-05-27 19:42:37 -0700

    Yay - one small step

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+    constructor(props) {
+      super(props) 
+      this.state={
+        leftTankThird: "..."
+      }
+
+    }
     calculateGas() {
       let leftTankPressure=document.getElementById("lTankPressure").value
-      console.log(Math.floor(leftTankPressure/3))
+      let leftTankThird=Math.floor(leftTankPressure/3)
+      console.log(leftTankThird)
+      this.setState({
+        leftTankThird: leftTankThird
+      })
     }
-    render( ){
+    render() {
       return (
         <div className="App">
             <h1>Sidemount Gas Calculator</h1>
@@ -15,22 +26,8 @@ class App extends React.Component {
               <input id="lTankPressure"></input>
             <p>Right Tank Pressure</p>
               <input id="rTankPressure"></input>
-            <p>Third of Left Tank</p>
+            <p>Third of Left Tank: {this.state.leftTankThird}</p>
             <div onClick={()=>{this.calculateGas()}} className="answerButton"> Click for Answer</div>
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
         </div>
       );
     }
